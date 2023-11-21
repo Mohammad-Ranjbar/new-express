815a9b3df37e9005ab842c4d9835e87821b6aaec
Mohammad Javad Ranjbar <mj.ranjbar.94@gmail.com>
2023-11-21 15:36:08 +0330

    prisma

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,37 @@
 import express, {Express, Request, Response} from 'express';
 import 'dotenv/config'
+import mysql, {Pool} from 'mysql2';
 
-const app:Express = express();
+const app: Express = express();
 
-app.get('/', (req: Request, res: Response):void => {
-    res.send('Hello, World!');
+app.use(express.json());
+
+
+const connection: Pool = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "1111",
+    database: "express",
 });
+
+app.post("/users", async (req: Request, res: Response) => {
+    try {
+        const {name, address, country} = req.body;
+        const [{insertId}] = await connection.promise().query(
+            `INSERT INTO users (name, address, country)
+             VALUES (?, ?, ?)`,
+            [name, address, country]
+        );
+        res.status(202).json({
+            message: "User Created",
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: err,
+        });
+    }
+});
+
 
 const PORT: string | 3000 = process.env.PORT || 3000;
 
