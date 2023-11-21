d2c450c705b3d8b050c1b655676a445ba4696c4e
Mohammad Javad Ranjbar <mj.ranjbar.94@gmail.com>
2023-11-21 10:56:16 +0330

    faet(ENV)

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import express, { Request, Response } from 'express';
+import express, {Express, Request, Response} from 'express';
+import 'dotenv/config'
 
+const app:Express = express();
 
-const app = express();
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
+app.get('/', (req: Request, res: Response):void => {
+    res.send('Hello, World!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT: string | 3000 = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
